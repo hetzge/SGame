e93a372f1ce1f407f2e494a6c6edda0d41acbb07
hetzge <hetzge@gmx.de>
2015-01-24 12:59:19 +0100

    Update

--- SGame_Entity/src/de/hetzge/sgame/entity/module/ValueModule.java
+++ SGame_Entity/src/de/hetzge/sgame/entity/module/ValueModule.java
@@ -1,29 +1,9 @@
 package de.hetzge.sgame.entity.module;
 
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-
 import de.hetzge.sgame.entity.BaseEntityModule;
 import de.hetzge.sgame.entity.Entity;
 
 public class ValueModule extends BaseEntityModule {
-
-	private class Container implements Serializable {
-
-		private class Value implements Serializable {
-			int available;
-			int max;
-		}
-
-		private final Map<Object, Integer> valueByKey = new HashMap<>();
-		private final Map<Object, Integer> maxByKey = new HashMap<>();
-
-		private boolean add(Object key, int amount) {
-
-		}
-
-	}
 
 	public ValueModule(Entity entity) {
 		super(entity);
